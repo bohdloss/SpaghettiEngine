ce4576ee20fa5203d21351961ed5048d3b1f31fa
BuildTools <unconfigured@null.spigotmc.org>
2021-07-11 23:45:10 +0200

    Fixed broken streaming audio when packaging to jar

--- SpaghettiEngine/src/main/java/com/spaghetti/assets/MusicLoader.java
+++ SpaghettiEngine/src/main/java/com/spaghetti/assets/MusicLoader.java
@@ -1,5 +1,6 @@
 package com.spaghetti.assets;
 
+import java.io.BufferedInputStream;
 import java.nio.ByteOrder;
 
 import javax.sound.sampled.AudioFormat;
@@ -26,51 +27,60 @@ public class MusicLoader implements AssetLoader {
 
 	@Override
 	public Object[] loadAsset(SheetEntry asset) throws Throwable {
-		AudioInputStream audio_stream = AudioSystem.getAudioInputStream(ResourceLoader.getStream(asset.args[0]));
-		AudioFormat audio_format = audio_stream.getFormat();
+		AudioInputStream audio_stream = null;
+		try {
+			audio_stream = AudioSystem
+					.getAudioInputStream(new BufferedInputStream(ResourceLoader.getStream(asset.args[0])));
+			AudioFormat audio_format = audio_stream.getFormat();
 
-		StreamProvider provider = () -> AudioSystem.getAudioInputStream(ResourceLoader.getStream(asset.args[0]));
+			StreamProvider provider = () -> AudioSystem
+					.getAudioInputStream(new BufferedInputStream(ResourceLoader.getStream(asset.args[0])));
 
-		// Read metadata
-		int channels = audio_format.getChannels();
-		int bps = audio_format.getSampleSizeInBits();
-		int samplerate = (int) audio_format.getSampleRate();
+			// Read metadata
+			int channels = audio_format.getChannels();
+			int bps = audio_format.getSampleSizeInBits();
+			int samplerate = (int) audio_format.getSampleRate();
 
-		int format = AL10.AL_FORMAT_MONO8;
-		if (channels == 1) {
-			if (bps == 8) {
-				format = AL10.AL_FORMAT_MONO8;
-			} else if (bps == 16) {
-				format = AL10.AL_FORMAT_MONO16;
+			int format = AL10.AL_FORMAT_MONO8;
+			if (channels == 1) {
+				if (bps == 8) {
+					format = AL10.AL_FORMAT_MONO8;
+				} else if (bps == 16) {
+					format = AL10.AL_FORMAT_MONO16;
+				}
+			} else if (channels == 2) {
+				if (bps == 8) {
+					format = AL10.AL_FORMAT_STEREO8;
+				} else if (bps == 16) {
+					format = AL10.AL_FORMAT_STEREO16;
+				}
 			}
-		} else if (channels == 2) {
-			if (bps == 8) {
-				format = AL10.AL_FORMAT_STEREO8;
-			} else if (bps == 16) {
-				format = AL10.AL_FORMAT_STEREO16;
+
+			int numbuffers = 0;
+			int buffersize = 0;
+
+			// Optional argument numbuffers
+			if (asset.args.length >= 2) {
+				try {
+					numbuffers = Integer.parseInt(asset.args[1]);
+				} catch (NumberFormatException e) {
+				}
+			}
+
+			// Optional argument buffersize
+			if (asset.args.length >= 3) {
+				try {
+					buffersize = Integer.parseInt(asset.args[2]);
+				} catch (NumberFormatException e) {
+				}
+			}
+
+			return new Object[] { format, samplerate, bps, ByteOrder.LITTLE_ENDIAN, provider, numbuffers, buffersize };
+		} finally {
+			if (audio_stream != null) {
+				audio_stream.close();
 			}
 		}
-
-		int numbuffers = 0;
-		int buffersize = 0;
-
-		// Optional argument numbuffers
-		if (asset.args.length >= 2) {
-			try {
-				numbuffers = Integer.parseInt(asset.args[1]);
-			} catch (NumberFormatException e) {
-			}
-		}
-
-		// Optional argument buffersize
-		if (asset.args.length >= 3) {
-			try {
-				buffersize = Integer.parseInt(asset.args[2]);
-			} catch (NumberFormatException e) {
-			}
-		}
-
-		return new Object[] { format, samplerate, bps, ByteOrder.LITTLE_ENDIAN, provider, numbuffers, buffersize };
 	}
 
 	@Override
